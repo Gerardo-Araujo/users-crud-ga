882577e47d10c394bfdff472532cb6431d505663
GerardoAraujo <gerardo.araujos@gmail.com>
2024-05-05 12:45:34 -0500

    first commit

---
+++ src/controllers/users.controllers.js
@@ -0,0 +1,54 @@
+const catchError = require('../utils/catchError');
+const Users = require('../models/Users');
+
+const getAll = catchError(async(req, res) => {
+    // Operaciones...
+    const users = await Users.findAll();
+    return res.json(users);
+});
+
+const create = catchError(async(req, res) => {
+    const { first_name, last_name, email, password, birthday } = req.body;
+    const users =  await Users.create({
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        password: password,
+        birthday: birthday,
+   });
+
+   return res.status(201).json(users);
+});
+
+const  getOne = catchError(async(req, res) => {
+    const {id} = req.params;
+    const users =  await Users.findByPk(id);
+    return res.json(users);
+});
+
+const  remove = catchError(async(req, res) => {
+    const {id} = req.params;
+    await Users.destroy({ where: { id:id }});
+    return res.sendStatus(204);
+})
+
+const  update = catchError(async(req, res) => {
+    const {first_name, last_name, email, password, birthday } = req.body;
+    const {id} = req.params;
+    const users =  await Users.update({
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        password: password,
+        birthday: birthday,
+        }, { where: { id:id}, returning: true});
+    return res.json(users);
+});
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    remove,
+    update
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const usersRouter = require('./users.router')
 const router = express.Router();
 
 // colocar las rutas aquí
-
+router.use(usersRouter)
 
 module.exports = router;
+
+
